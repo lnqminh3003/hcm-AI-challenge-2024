774f4d066c2247fd55a2a669a56fd199750969ec
LMHoang20 <leminhhoang7554@gmail.com>
2024-10-14 10:03:18 +0700

    fix compress decompress frame list

--- aic-24-BE/utils.py
+++ aic-24-BE/utils.py
@@ -188,6 +188,7 @@ def print_log(text, file_path):
 def asc_list_compress(l: list[int]) -> str:
     if len(l) == 0:
         return ""
+    l = sorted(set(l))
     c = [l[0]]
     for i in range(1, len(l)):
         c.append(l[i] - l[i - 1])
@@ -200,6 +201,7 @@ def asc_list_decompress(s: str) -> list[int]:
     l = [c[0]]
     for i in range(1, len(c)):
         l.append(l[i - 1] + c[i])
+    l = list(map(lambda x: str(x).zfill(5), l))
     return l
 
 def unicode_string_compress(s: str) -> str:
